085fdc55dd33ade4d37d125e67cad3741f798194
Ameer Faidullah Salah <ameer.faidullah.salah@hp.com>
2025-01-19 00:11:15 +0200

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,24 @@ import "./App.css";
 import Counter from "./Component/Counter";
 
 const App: React.FC = () => (
-  <>
-    <div>
-      <div
-        style={{
-          fontFamily: "Arial, sans-serif",
-          padding: "2rem",
-          margin: "auto",
-        }}
-      >
-        <h1>React Design Patterns: Container and Presentation</h1>
-        <UserProfileContainer />
-      </div>
-      <h1>Component Composition with Hooks</h1>
+  <div style={{ textAlign: "center", width: "100%" }}>
+    <h1>React Design Patterns:</h1>
+    <div
+      className="container"
+      style={{
+        fontFamily: "Arial, sans-serif",
+        padding: "2rem",
+        margin: "auto",
+      }}
+    >
+      <h2> Container and Presentation</h2>
+      <UserProfileContainer />
+    </div>
+    <div className="container">
+      <h2>Component Composition with Hooks</h2>
       <Counter />
     </div>
-  </>
+  </div>
 );
 
 export default App;
